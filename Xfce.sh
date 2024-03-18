8d5a003c51deaca48625b620973ddb887c2b293f
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-03-18 20:28:34 +0600

    Update Xfce.sh

--- Xfce.sh
+++ Xfce.sh
@@ -4,3 +4,7 @@ sudo apt install xfce4 xfce4-whiskermenu-plugin xfce4-terminal -y
 echo "export DISPLAY=:1" >> /bin/startxfce
 echo "xfce4-session" >> /bin/startxfce
 chmod +x /bin/startxfce
+useradd -m -s /bin/bash debian
+echo "debian:debian" | chpasswd
+echo "debian  ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/debian
+exit
