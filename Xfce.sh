5653642652e524cafece8e75c4047613705f0c48
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-03-18 20:25:14 +0600

    Update Xfce.sh

--- Xfce.sh
+++ Xfce.sh
@@ -1,3 +1,6 @@
 apt update
 apt install sudo wget -y
 sudo apt install xfce4 xfce4-whiskermenu-plugin xfce4-terminal -y
+echo "export DISPLAY=:1" >> /bin/startxfce
+echo "xfce4-session" >> /bin/startxfce
+chmod +x /bin/startxfce
