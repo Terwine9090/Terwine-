57c0b43c44f48c9f3585be5ced5230410aa65826
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-11 13:35:06 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,7 +3,7 @@ apt upgrade -y
 apt install wget proot -y
 apt install x11-repo
 apt install termux-x11-nightly -y
-apt install virglrenderer-andriod -y
+apt install virglrenderer-android -y
 echo "download rootfs"
 wget https://github.com/Terwine9090/ubuntu-wine/releases/download/v1.0.0/rootfs.tar.xz
 echo "extract rootfs"
