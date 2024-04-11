6d9fc5bc0bc7fca3149385f1fa24a0ccf45a8568
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-11 21:28:37 +0600

    Update Proot.sh

--- Proot.sh
+++ Proot.sh
@@ -1,9 +1,19 @@
 virgl_test_server_android &
 unset LD_PRELOAD
-cmdline="proot --link2symlink  ubuntu-fs -w /root -b /dev -b /proc -b /sys -b $PREFIX/tmp:/tmp -b /storage \
-         /usr/bin/env -i HOME=/root TERM=$TERM PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games LANG=C.UTF-8 /bin/bash --login"
-         
-          
+cmdline="proot \
+        --link2symlink \
+        -0 \
+        -r ubuntu \
+        -b /dev \
+        -b /proc \
+        -b ubuntu/root:/dev/shm \
+        -w /root \
+           /usr/bin/env -i \
+           HOME=/root \
+           PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin \
+           TERM=$TERM \
+           LANG=C.UTF-8 \
+           bash"
 cmd="$@"
 if [ "$#" == "0" ];then
     exec $cmdline
