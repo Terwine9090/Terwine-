28b4128b87c679b49b056a5a27c69bf2dd03b126
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-11 21:43:15 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -11,11 +11,6 @@ tar -xf rootfs.tar.xz
 echo "download wine"
 wget https://raw.githubusercontent.com/Terwine9090/ubuntu-wine/main/Proot.sh
 rm -rf ubuntu/root/.bashrc
-echo 'export DISPLAY=:0
-export BOX86_PATH=~/wine/bin/
-export BOX86_LD_LIBRARY_PATH=~/wine/lib/wine/i386-unix/:/lib/i386-linux-gnu/:/lib/aarch64-linux-gnu/:/lib/arm-linux-gnueabihf/:/usr/lib/aarch64-linux-gnu/:/usr/lib/arm-linux-gnueabihf/:/usr/lib/i386-linux-gnu/
-export BOX64_PATH=~/wine64/bin/
-export BOX64_LD_LIBRARY_PATH=~/wine64/lib/i386-unix/:~/wine64/lib/wine/x86_64-unix/:/lib/i386-linux-gnu/:/lib/x86_64-linux-gnu:/lib/aarch64-linux-gnu/:/lib/arm-linux-gnueabihf/:/usr/lib/aarch64-linux-gnu/:/usr/lib/arm-linux-gnueabihf/:/usr/lib/i386-linux-gnu/:/usr/lib/x86_64-linux-gnu' >> ubuntu/root/.bashrc
 cd ubuntu/root
 wget https://github.com/Pi-Apps-Coders/files/releases/download/large-files/wine-8.14.tar.gz
 cd
@@ -29,3 +24,6 @@ apt install proot-distro -y
 mv ubuntu $PREFIX/var/lib/proot-distro/installed-rootfs
 echo "pd login ubuntu --shared-tmp" >> $PREFIX/bin/ubuntu
 chmod +x $PREFIX/bin/ubuntu
+wget https://raw.githubusercontent.com/Terwine9090/ubuntu-wine/main/menu
+rm -rf $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
+mv menu $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
