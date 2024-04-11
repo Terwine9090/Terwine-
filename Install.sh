4e4a560f7e5fedb79aba110bebbea85d6d2976fe
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-11 07:31:57 +0600

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,9 @@
+apt update
+apt upgrade -y
+apt install wget proot 
+echo "download rootfs"
+wget https://github.com/Terwine9090/ubuntu-wine/releases/download/v1.0.0/rootfs.tar.xz
+echo "extract rootfs"
+tar -xf rootfs.tar.xz
+echo "download wine"
+wget 
