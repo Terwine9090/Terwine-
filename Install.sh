6422f1ddc53e34cd89722414dcd07450725f517a
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-11 07:38:50 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -8,3 +8,9 @@ tar -xf rootfs.tar.xz
 echo "download wine"
 wget https://raw.githubusercontent.com/Terwine9090/ubuntu-wine/main/Proot.sh
 rm -rf ubuntu/root/.bashrc
+echo 'export DISPLAY=:0
+export BOX86_PATH=~/wine/bin/
+export BOX86_LD_LIBRARY_PATH=~/wine/lib/wine/i386-unix/:/lib/i386-linux-gnu/:/lib/aarch64-linux-gnu/:/lib/arm-linux-gnueabihf/:/usr/lib/aarch64-linux-gnu/:/usr/lib/arm-linux-gnueabihf/:/usr/lib/i386-linux-gnu/
+export BOX64_PATH=~/wine64/bin/
+export BOX64_LD_LIBRARY_PATH=~/wine64/lib/i386-unix/:~/wine64/lib/wine/x86_64-unix/:/lib/i386-linux-gnu/:/lib/x86_64-linux-gnu:/lib/aarch64-linux-gnu/:/lib/arm-linux-gnueabihf/:/usr/lib/aarch64-linux-gnu/:/usr/lib/arm-linux-gnueabihf/:/usr/lib/i386-linux-gnu/:/usr/lib/x86_64-linux-gnu' >> ubuntu/root/.bashrc
+1
