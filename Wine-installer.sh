519d0a45620125e45546b9f69fa1eba97fcb9067
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-17 17:37:10 +0600

    Update Wine-installer.sh

--- Wine-installer.sh
+++ Wine-installer.sh
@@ -1,8 +1,9 @@
-e
-
+if [ -e /root/.wine64 ]; then
 wget https://github.com/doitsujin/dxvk/releases/download/v2.1/dxvk-2.1.tar.gz
 tar -xvf dxvk-2.1.tar.gz
 cd dxvk-2.1
 cp x32/* ~/.wine64/drive_c/windows/system32
 cp x64/* ~/.wine64/drive_c/windows/syswow64
-
+fi
+rm -rf dxvk-2.1.tar.gz
+rm -rf wine-9.3.tar.gz
