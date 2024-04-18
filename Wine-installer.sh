a9164e904d41f71ff019b88203b84147dc730fce
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-18 16:14:53 +0600

    Update Wine-installer.sh

--- Wine-installer.sh
+++ Wine-installer.sh
@@ -1,9 +1,20 @@
-if [ -e /root/.wine64 ]; then
-wget https://github.com/doitsujin/dxvk/releases/download/v2.1/dxvk-2.1.tar.gz
+echo "your install dxvk to type the version"
+echo "2.3.1"
+echo "2.3"
+echo "2.2"
+echo "2.1"
+echo "2.0"
+echo "1.10.3"
+echo "1.10.2"
+echo "1.10.1"
+read -p ":" version
+wine wineboot
+wget https://github.com/doitsujin/dxvk/releases/download/v2.1/dxvk-${version}.tar.gz
 tar -xvf dxvk-2.1.tar.gz
 cd dxvk-2.1
 cp x32/* ~/.wine64/drive_c/windows/system32
 cp x64/* ~/.wine64/drive_c/windows/syswow64
-fi
+
+echo "clean"
 rm -rf dxvk-2.1.tar.gz
 rm -rf wine-9.3.tar.gz
