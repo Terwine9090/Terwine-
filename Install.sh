abfc30e8df39e35e4df4f44ccdd76638feed15de
Terwine9090 <163871667+Terwine9090@users.noreply.github.com>
2024-04-26 19:16:54 +0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -21,7 +21,6 @@ wget https://github.com/Pi-Apps-Coders/files/releases/download/large-files/wine-
 tar -xf wine-9.3.tar.gz
 mv wine-9.3 wine
 rm -rf wine-9.3.tar.gz
-wget https://raw.githubusercontent.com/Terwine9090/ubuntu-wine/main/menu
 wget https://raw.githubusercontent.com/Terwine9090/ubuntu-wine/main/wine-stop
 cd
 echo "creating command"
@@ -37,10 +36,6 @@ echo "fix audio"
 bash Proot.sh apt update
 bash Proot.sh apt install pulseaudio -y
 rm -rf ubuntu/root/.bashrc
-echo "export res=1280x720" >> ubuntu/root/.bashrc
-echo "export core=4-7" >> ubuntu/root/.bashrc
-echo "export DISPLAY=:0" >> ubuntu/root/.bashrc
-echo "bash /opt/menu" >> ubuntu/root/.bashrc
 echo "termux-x11 &> /dev/null & bash windroid.sh" >> $PREFIX/bin/winedroid
 chmod +x $PREFIX/bin/winedroid
 cd 
@@ -51,6 +46,7 @@ cd
 bash Proot.sh bash wine-util 
 rm -rf Proot.sh
 wget https://raw.githubusercontent.com/Terwine9090/ubuntu-wine/main/windroid.sh
+wget 
 echo "pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" >> .bashrc
 echo " to start winedroid to type winedroid"
